86118ae20e18a60ef1bcf390a50c460c83ca0316
AshrithaSimharaju <163991546+AshrithaSimharaju@users.noreply.github.com>
2024-12-11 04:48:39 +0000

    Update Quiz.java

--- Quiz/Quiz.java
+++ Quiz/Quiz.java
@@ -19,13 +19,6 @@ public class Quiz{
 
     public static void main(String[] args) {
         List<Question> questions = new ArrayList<>();
-
-    questions.add(new Question(
-    "What does JVM stand for in Java?",
-    new String[]{"1. Java Variable Machine", "2. Java Virtual Machine", "3. Java Value Module", "4. Java Version Manager"},
-    2
-    ));
-
     questions.add(new Question(
     "Which keyword is used to create a subclass in Java?",
     new String[]{"1. implements", "2. extends", "3. inherit", "4. subclass"},
@@ -54,27 +47,6 @@ public class Quiz{
         "What is the output of `System.out.println(10 + 20 + \"Java\");`?",
         new String[]{"1. 30Java", "2. Java30", "3. 10Java20", "4. Compilation Error"},
         1
-        ));
-
-        questions.add(new Question(
-        "Which of these is not a feature of Java?",
-        new String[]{"1. Object-Oriented", "2. Platform-Dependent", "3. Multithreaded", "4. Robust"},
-        2
-        ));
-        questions.add(new Question(
-            "Which planet is known as the Red Planet?",
-            new String[]{"1. Earth", "2. Mars", "3. Jupiter", "4. Venus"},
-            2
-        ));
-        questions.add(new Question(
-            "Who developed the theory of relativity?",
-            new String[]{"1. Isaac Newton", "2. Albert Einstein", "3. Galileo Galilei", "4. Nikola Tesla"},
-            2
-        ));
-        questions.add(new Question(
-            "Which element has the chemical symbol 'O'?",
-            new String[]{"1. Oxygen", "2. Gold", "3. Osmium", "4. Opal"},
-            1
         ));
         Collections.shuffle(questions);
 
